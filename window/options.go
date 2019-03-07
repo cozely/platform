f03067a73fe0e72ec5ff278cf2b02bfafdeca594
drakmaniso <moussault.laurent@gmail.com>
2019-03-07 21:14:06 +0100

    Reorganize package window

---
+++ window/options.go
@@ -0,0 +1,69 @@
+package window
+
+import "errors"
+
+type Option func(*Window) error
+
+func Title(s string) Option {
+	return func(w *Window) error {
+		if w.opened {
+			//TODO: implement
+			return errors.New("internal.VSync: not implemented for opened windows")
+		}
+		w.title = s
+		return nil
+	}
+}
+
+func Size(x, y int32) Option {
+	return func(w *Window) error {
+		if w.opened {
+			//TODO: implement
+			return errors.New("internal.VSync: not implemented for opened windows")
+		}
+		w.size = Coord{x, y}
+		return nil
+	}
+}
+
+func Fullscreen(fullscreen bool, windowed bool) Option {
+	return func(w *Window) error {
+		if w.opened {
+			//TODO: implement
+			return errors.New("internal.VSync: not implemented for opened windows")
+		}
+		w.fullscreen = fullscreen
+		w.desktop = !windowed
+		return nil
+	}
+}
+
+func Monitor(n int) Option {
+	return func(w *Window) error {
+		if w.opened {
+			return errors.New("internal.VSync: not implemented for opened windows")
+		}
+		w.monitor = int32(n)
+		return nil
+	}
+}
+
+func VSync(enable bool) Option {
+	return func(w *Window) error {
+		if w.opened {
+			return errors.New("internal.VSync: not implemented for opened windows")
+		}
+		w.vsync = enable
+		return nil
+	}
+}
+
+func Debug(enable bool) Option {
+	return func(w *Window) error {
+		if w.opened {
+			return errors.New("internal.Debug: not implemented for opened windows")
+		}
+		w.debug = enable
+		return nil
+	}
+}
--- window/window.go
+++ window/window.go
@@ -4,7 +4,6 @@
 package window
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/cozely/platform/internal"
@@ -85,75 +84,7 @@ func (w *Window) HasMouseFocus() bool {
 	return w.hasMouseFocus
 }
 
-var size Coord
-
 // Size returns the size of the window in (screen) pixels.
 func (w *Window) Size() Coord {
 	return w.size
-}
-
-type Option func(*Window) error
-
-func Title(s string) Option {
-	return func(w *Window) error {
-		if w.opened {
-			//TODO: implement
-			return errors.New("internal.VSync: not implemented for opened windows")
-		}
-		w.title = s
-		return nil
-	}
-}
-
-func Size(x, y int32) Option {
-	return func(w *Window) error {
-		if w.opened {
-			//TODO: implement
-			return errors.New("internal.VSync: not implemented for opened windows")
-		}
-		w.size = Coord{x, y}
-		return nil
-	}
-}
-
-func Fullscreen(fullscreen bool, windowed bool) Option {
-	return func(w *Window) error {
-		if w.opened {
-			//TODO: implement
-			return errors.New("internal.VSync: not implemented for opened windows")
-		}
-		w.fullscreen = fullscreen
-		w.desktop = !windowed
-		return nil
-	}
-}
-
-func Monitor(n int) Option {
-	return func(w *Window) error {
-		if w.opened {
-			return errors.New("internal.VSync: not implemented for opened windows")
-		}
-		w.monitor = int32(n)
-		return nil
-	}
-}
-
-func VSync(enable bool) Option {
-	return func(w *Window) error {
-		if w.opened {
-			return errors.New("internal.VSync: not implemented for opened windows")
-		}
-		w.vsync = enable
-		return nil
-	}
-}
-
-func Debug(enable bool) Option {
-	return func(w *Window) error {
-		if w.opened {
-			return errors.New("internal.Debug: not implemented for opened windows")
-		}
-		w.debug = enable
-		return nil
-	}
 }
